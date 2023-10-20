7da34575bbbd722f5bf6137883ab9899da2955fa
Amelia Jones <jonesas@hendrix.edu>
2023-10-20 00:21:55 -0500

    Searching

--- RazorPagesGame/Pages/Games/Index.cshtml.cs
+++ RazorPagesGame/Pages/Games/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesGame.Data;
 using RazorPagesGame.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 
 namespace RazorPagesGame.Pages_Games
 {
@@ -21,12 +23,47 @@ namespace RazorPagesGame.Pages_Games
 
         public IList<Game> Game { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? GameGenre { get; set; }
+
+        public SelectList? Platforms { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? GamePlatform { get; set; }
+
         public async Task OnGetAsync()
-        {
-            if (_context.Game != null)
-            {
-                Game = await _context.Game.ToListAsync();
+        {   
+            IQueryable<string> genreQuery = from g in _context.Game
+                                            orderby g.Genre
+                                            select g.Genre;
+
+            IQueryable<string> platformQuery = from g in _context.Game
+                                            orderby g.Platform
+                                            select g.Platform;
+
+            var games = from g in _context.Game
+                 select g;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {   
+                games = games.Where(s => s.Title.Contains(SearchString));
             }
+
+            if(!string.IsNullOrEmpty(GameGenre)){
+                games = games.Where(x => x.Genre == GameGenre);
+            }
+            if(!string.IsNullOrEmpty(GamePlatform)){
+                games = games.Where(p => p.Platform == GamePlatform);
+            }
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+                Platforms = new SelectList(await platformQuery.Distinct().ToListAsync());
+                Game = await games.ToListAsync();
+            
         }
     }
 }

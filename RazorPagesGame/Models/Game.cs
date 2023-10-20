9c78bfa17804616f82702d75663458f6edceb6fe
Amelia Jones <jonesas@hendrix.edu>
2023-10-20 00:34:37 -0500

    Validation

--- RazorPagesGame/Models/Game.cs
+++ RazorPagesGame/Models/Game.cs
@@ -6,14 +6,27 @@ namespace RazorPagesGame.Models;
 public class Game
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; } = string.Empty;
 
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
-    public string? Platform{ get; set;}
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(20)]
+    [Required]
+    public string Platform{ get; set;} = string.Empty;
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
